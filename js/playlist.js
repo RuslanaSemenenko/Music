292ff15ce804533d9ebfeee2d2f37d0036e670b9
Ruslana Semenenko <katotochkaaa@gmail.com>
2024-01-19 17:06:28 +0000

    work

---
+++ js/playlist.js
@@ -0,0 +1,38 @@
+let songs = [
+  {
+    name: "Call out my name",
+    artist: "The Weeknd",
+    img: "img1-min",
+    audio: "The-Weeknd-Call-out-my-name",
+  },
+  {
+    name: "You dont own me",
+    artist: "SAYGRACE",
+    img: "img2-min",
+    audio: "You_dont_own_me_-_SAYGRACE",
+  },
+  {
+    name: "Swimming pools",
+    artist: "Kendrick Lamar",
+    img: "img3-min",
+    audio: "Kendrick_lamar_Swimming_pools",
+  },
+  {
+    name: "Used to be young",
+    artist: "Miley Cyrus",
+    img: "img4-min",
+    audio: "Miley_Cyrus_-_Used_to_be_young",
+  },
+  {
+    name: "Stay with me",
+    artist: "Sam Іmith",
+    img: "img5-min",
+    audio: "Sam-smith-stay-with-me",
+  },
+  {
+    name: "Island",
+    artist: "Miley Cyrus",
+    img: "img6-min",
+    audio: "Miley_Cyrus_-_Island",
+  },
+];
